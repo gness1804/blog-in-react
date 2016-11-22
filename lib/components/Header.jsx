ab8d4e9b61e251dd7ba32fdfb68e158076b662e8
gness1804 <graham.nessler@gmail.com>
2016-11-22 14:02:11 -0500

    setting up links for header

--- lib/components/Header.jsx
+++ lib/components/Header.jsx
@@ -1,11 +1,27 @@
 import React, {Component} from 'react';
+import { Link } from 'react-router';
+
 const ReactDOM = require('react-dom');
 
 class Header extends Component {
 
   render() {
     return (
-      <div>I am the header.</div>
+      <div>
+        <header>
+		    	<h1>Graham Nessler</h1>
+		      <h3>Aspiring developer, scholar, lover of cats and dogs.</h3>
+		      <ul className="mainmenu">
+						<Link to="/" className="link1 current-page">
+						  <li>Home</li>
+						</Link>
+						<Link to="/aboutme" className="link1">
+							<li>About Me</li>
+						</Link>
+						<a className="link1" href="http://www.grahamnessler.com"><li>Portfolio</li></a>
+					</ul>
+				</header>
+      </div>
     );
   }
 
