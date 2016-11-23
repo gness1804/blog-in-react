1c62a8e9c8eef3640f908530e70e450f84dfa649
gness1804 <graham.nessler@gmail.com>
2016-11-23 16:30:54 -0500

    solves top links rendering problem

--- lib/components/EachPost.jsx
+++ lib/components/EachPost.jsx
@@ -4,16 +4,23 @@ class EachPost extends Component {
 
   render() {
 
+    let topLinks;
+
     const { title, date, image, note, body, github, interactiveLink, secondPara, thirdPara } = this.props;
+
+    if (github || interactiveLink) {
+      topLinks = <div className="links-container-top">
+        {github ? <a href={github}><h3 className="github">Github</h3></a> : ''}
+        {interactiveLink ? <a href={interactiveLink}><h3 className="check-it-out">Check it Out!</h3></a> : ''}
+      </div>;
+    }
+
 
     return (
       <div className="template">
 			  <h2>{title}</h2>
 			   <p className="date">Posted on {date}</p>
-         <div className="links-container-top">
-           {github ? <a href={github}><h3 className="github">Github</h3></a> : ''}
-           {interactiveLink ? <a href={interactiveLink}><h3 className="check-it-out">Check it Out!</h3></a> : ''}
-         </div>
+         {topLinks}
 				 <img src={image} alt="The image for the topic."></img>
                                  { note ? <h4 className="note">{note}</h4> : ''}
 				 <p className="body">{body}</p>
