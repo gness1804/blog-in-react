e9cab0a932477e8da46a1bea010d8d4d230f615b
gness1804 <graham.nessler@gmail.com>
2016-11-23 15:14:04 -0500

    first two poses are done

--- lib/components/EachPost.jsx
+++ lib/components/EachPost.jsx
@@ -4,19 +4,19 @@ class EachPost extends Component {
 
   render() {
 
-    const { title, date, image, note, body, github, interactiveLink } = this.props;
+    const { title, date, image, note, body, github, interactiveLink, secondPara, thirdPara } = this.props;
 
     return (
       <div className="template">
 			  <h2>{title}</h2>
-			   <p className="date">{date}</p>
+			   <p className="date">Posted on {date}</p>
          {github ? <a href={github}><h3>Github</h3></a> : ''}
          {interactiveLink ? <a href={interactiveLink}><h3>Check it Out!</h3></a> : ''}
 				 <img src={image} alt="The image for the topic."></img>
                                  { note ? <h4 className="note">{note}</h4> : ''}
 				 <p className="body">{body}</p>
-         <p className="second-para">Foo</p>
-         <p className="third-para">Foo</p>
+         {secondPara ? <p className="second-para">{secondPara}</p> : ''}
+         {thirdPara ? <p className="third-para">{thirdPara}</p> : ''}
 		</div>
     );
   }
