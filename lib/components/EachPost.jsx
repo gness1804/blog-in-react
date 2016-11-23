298a71dae3d5a08b0f7abe3e8bae858d8f19b785
gness1804 <graham.nessler@gmail.com>
2016-11-23 12:58:03 -0500

    adds spaces for github and interactive link in EachPost

--- lib/components/EachPost.jsx
+++ lib/components/EachPost.jsx
@@ -1,15 +1,17 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class EachPost extends Component {
 
   render() {
 
-    const { title, date, image, note, body } = this.props;
+    const { title, date, image, note, body, github, interactiveLink } = this.props;
 
     return (
       <div className="template">
 			  <h2>{title}</h2>
 			   <p className="date">{date}</p>
+         {github ? <a href={github}><h3>Github</h3></a> : ''}
+         {interactiveLink ? <a href={interactiveLink}><h3>Check it Out!</h3></a> : ''}
 				 <img src={image} alt="The image for the topic."></img>
 				 <h4 className="note">{note}</h4>
 				 <p className="body">{body}</p><br></br>
