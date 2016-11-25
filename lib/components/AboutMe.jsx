2508a78acef581804b438b5604544c9d066880aa
gness1804 <graham.nessler@gmail.com>
2016-11-25 13:33:08 -0500

    deploying to gh-pages

--- lib/components/AboutMe.jsx
+++ lib/components/AboutMe.jsx
@@ -8,7 +8,7 @@ class AboutMe extends Component {
         <div className="empty-space"></div>
         <div className="template">
 			    <h2>About Me</h2>
-			    <img src="../../images/headshot.jpg" alt="Headshot."></img>
+			    <img src="./headshot.jpg" alt="Headshot."></img>
 			    <p>I am originally from Bryan/College Station, Texas, and am currently located in Denver. In my first life, I was a <a href="https://sites.google.com/site/grahamnessler1/home" target="blank">scholar of Caribbean and Latin American history</a>, and I am now training to become a web developer. You can find out more in <a href="http://www.grahamnessler.com" target="blank">my portfolio</a>. Go Blue!</p>
 		    </div>
     </div>
