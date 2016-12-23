cc1489cc9463ddccf434623946c3859891237501
gness1804 <graham.nessler@gmail.com>
2016-12-22 23:27:40 -0700

    creates and formats back home button in about me page

--- lib/components/AboutMe.jsx
+++ lib/components/AboutMe.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { Link } from 'react-router';
 
 class AboutMe extends Component {
 
@@ -11,6 +12,9 @@ class AboutMe extends Component {
 			    <img src="./headshot.jpg" alt="Headshot."></img>
 			    <p>I am originally from Bryan/College Station, Texas, and am currently located in Denver. In my first life, I was a <a href="https://sites.google.com/site/grahamnessler1/home" target="blank">scholar of Caribbean and Latin American history</a>, and I am now training to become a web developer. You can find out more in <a href="http://www.grahamnessler.com" target="blank">my portfolio</a>. Go Blue!</p>
 		    </div>
+        <Link className="back-home-button-link" to="/">
+          <button className="back-home-button">Back Home</button>
+        </Link>
     </div>
     );
   }
