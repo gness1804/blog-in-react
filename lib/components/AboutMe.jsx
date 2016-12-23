a7ca7a4ca8ebe593df4f806c964e4ecd4d9826c8
gness1804 <graham.nessler@gmail.com>
2016-12-22 22:57:52 -0700

    removes sidebar

--- lib/components/AboutMe.jsx
+++ lib/components/AboutMe.jsx
@@ -4,7 +4,7 @@ class AboutMe extends Component {
 
   render() {
     return (
-      <div>
+      <div className="master-container">
         <div className="empty-space"></div>
         <div className="template">
 			    <h2>About Me</h2>
